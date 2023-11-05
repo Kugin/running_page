cc1fdb20eb8d4f5750f799d100af4dbab999b63a
Kugin Chen <ctkugin@gmail.com>
2023-11-05 10:29:35 +0800

    feat: merge from master

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,9 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
-  // Instead, manually add a new token and apply URL restrictions.
-  // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoiY3RrdWdpbiIsImEiOiJjbG9rdTltZWQwOHdyMmxzNnByMWp0dDV6In0.g1I0Xy6JqqlD7ZFv98xVpQ';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
@@ -97,7 +94,8 @@ export {
   LIGHTS_ON,
 };
 
-const nike = 'rgb(224,237,94)'; // if you want change the main color change here src/styles/variables.scss
+// 224,237,94
+const nike = 'rgb(27,193,7)'; // if you want change the main color change here src/styles/variables.scss
 const dark_vanilla = 'rgb(228,212,220)';
 
 // If your map has an offset please change this line
